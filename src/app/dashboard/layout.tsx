0aaf278e6c08993c4cdd9aa63cdc376c2f2c8ebe
Dai <dai.phan@southasiaservices.com>
2025-05-12 10:41:42 +0700

    chore: update seller dashboard ui and command prisma db

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,19 @@
-import { ThemeProvider } from "next-themes";
+import Header from "@/components/dashboard/header/header";
+import Sidebar from "@/components/dashboard/sidebar/sidebar";
 import { ReactNode } from "react";
 
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+export default function SellerDashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
-    <div>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="light"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+    <div className="w-full h-full">
+      <Sidebar />
+      <div className="ml-[300px]">
+        <Header />
+        <div className="w-full mt-[75px] p-4">{children}</div>
+      </div>
     </div>
   );
 }
